ce393ed415e39d3562fea526f9192aa829ace6a7
mdmoreau <mdmoreau@gmail.com>
2023-07-04 17:04:55 -0400

    significant rework

--- index.js
+++ index.js
@@ -8,52 +8,142 @@ export default (root) => {
 
   let active = [];
 
-  const align = () => {
+  const getAlignment = () => {
     return getComputedStyle(items[0]).getPropertyValue('scroll-snap-align');
   };
 
-  const nav = (index) => {
+  const getCenter = () => {
+    const direction = getDirection();
+    const style = getComputedStyle(scroll);
+    const rtl = direction === 'row' && style.getPropertyValue('direction') === 'rtl';
+
+    let current, start, end, offset, target;
+
+    if (direction === 'row') {
+      current = scroll.scrollLeft;
+      start = parseInt(style.getPropertyValue('scroll-padding-inline-start'), 10) || 0;
+      end = parseInt(style.getPropertyValue('scroll-padding-inline-end'), 10) || 0;
+      offset = (scroll.offsetWidth - start - end) / 2;
+    }
+
+    if (direction === 'column') {
+      current = scroll.scrollTop;
+      start = parseInt(style.getPropertyValue('scroll-padding-block-start'), 10) || 0;
+      end = parseInt(style.getPropertyValue('scroll-padding-block-end'), 10) || 0;
+      offset = (scroll.offsetHeight - start - end) / 2;
+    }
+
+    target = current + (rtl ? end : start) + offset;
+
+    return getClosest(target);
+  };
+
+  const getClosest = (target) => {
+    const direction = getDirection();
+
+    const proximities = active.map((index) => {
+      const item = items[index];
+
+      let center, proximity;
+
+      if (direction === 'row') {
+        center = item.offsetLeft + item.offsetWidth / 2;
+      }
+
+      if (direction === 'column') {
+        center = item.offsetTop + item.offsetHeight / 2;
+      }
+
+      proximity = Math.abs(center - target);
+
+      return { index, proximity };
+    });
+
+    return proximities.sort((a, b) => a.proximity - b.proximity)[0].index;
+  };
+
+  const getDirection = () => {
+    return getComputedStyle(scroll).getPropertyValue('flex-direction');
+  };
+
+  const getIndex = () => {
+    const alignment = getAlignment();
+
+    if (alignment === 'start') return active[0];
+    if (alignment === 'end') return active[active.length - 1];
+    if (alignment === 'center') return getCenter();
+  };
+
+  const getPosition = (index) => {
+    const alignment = getAlignment();
+    const direction = getDirection();
+    const item = items[index];
+    const style = getComputedStyle(scroll);
+    const rtl = direction === 'row' && style.getPropertyValue('direction') === 'rtl';
+
+    let target, offset, start, end;
+
+    if (direction === 'row') {
+      target = item.offsetLeft;
+      start = parseInt(style.getPropertyValue('scroll-padding-inline-start'), 10) || 0;
+      end = parseInt(style.getPropertyValue('scroll-padding-inline-end'), 10) || 0;
+      offset = scroll.offsetWidth - item.offsetWidth + (rtl ? end - start : start - end);
+    }
+
+    if (direction === 'column') {
+      target = item.offsetTop;
+      start = parseInt(style.getPropertyValue('scroll-padding-block-start'), 10) || 0;
+      end = parseInt(style.getPropertyValue('scroll-padding-block-end'), 10) || 0;
+      offset = scroll.offsetHeight - item.offsetHeight + start - end;
+    }
+
+    if (alignment === 'start') return rtl ? target + end - offset : target - start;
+    if (alignment === 'end') return rtl ? target - start : target + end - offset;
+    if (alignment === 'center') return target - offset / 2;
+  };
+
+  const getProgress = () => {
+    const direction = getDirection();
+
+    let current, max;
+
+    if (direction === 'row') {
+      current = Math.abs(scroll.scrollLeft);
+      max = scroll.scrollWidth - scroll.clientWidth;
+    }
+
+    if (direction === 'column') {
+      current = scroll.scrollTop;
+      max = scroll.scrollHeight - scroll.clientHeight;
+    }
+
+    return Math.round(current / max * 100) / 100;
+  };
+
+  const setDisabled = () => {
+    const progress = getProgress();
+
+    if (progress === 0) {
+      prev?.setAttribute('disabled', '');
+    } else {
+      prev?.removeAttribute('disabled');
+    }
+
+    if (progress === 1) {
+      next?.setAttribute('disabled', '');
+    } else {
+      next?.removeAttribute('disabled');
+    }
+  };
+
+  const setScroll = (index) => {
     const item = items[index];
 
     if (item) {
-      const dir = getComputedStyle(scroll).getPropertyValue('flex-direction');
-      const alignment = align();
-
-      let x, y;
-
-      if (dir === 'row') {
-        const start = parseInt(getComputedStyle(scroll).getPropertyValue('scroll-padding-left'), 10);
-        const end = parseInt(getComputedStyle(scroll).getPropertyValue('scroll-padding-right'), 10);
-
-        switch (alignment) {
-          case 'start':
-            x = item.offsetLeft - start;
-            break;
-          case 'end':
-            x = item.offsetLeft + end - (scroll.offsetWidth - item.offsetWidth);
-            break;
-          case 'center':
-            x = item.offsetLeft - (scroll.offsetWidth - item.offsetWidth) / 2;
-            break;
-        }
-      }
-
-      if (dir === 'column') {
-        const start = parseInt(getComputedStyle(scroll).getPropertyValue('scroll-padding-top'), 10);
-        const end = parseInt(getComputedStyle(scroll).getPropertyValue('scroll-padding-bottom'), 10);
-
-        switch (alignment) {
-          case 'start':
-            y = item.offsetTop - start;
-            break;
-          case 'end':
-            y = item.offsetTop + end - (scroll.offsetHeight - item.offsetHeight);
-            break;
-          case 'center':
-            y = item.offsetTop - (scroll.offsetHeight - item.offsetHeight) / 2;
-            break;
-        }
-      }
+      const direction = getDirection();
+      const position = getPosition(index);
+      const x = direction === 'row' ? position : 0;
+      const y = direction === 'column' ? position : 0;
 
       scroll.scroll(x, y);
     }
@@ -63,26 +153,21 @@ export default (root) => {
     entries.forEach((entry) => {
       const { target, isIntersecting } = entry;
       const index = [...items].indexOf(target);
-      const start = index === 0;
-      const end = index === items.length - 1;
 
       if (isIntersecting) {
         active.push(index);
         target.removeAttribute('inert');
         target.setAttribute('data-oversnap-item', 'active');
         pages[index]?.setAttribute('data-oversnap-page', 'active');
-        start && prev?.setAttribute('disabled', '');
-        end && next?.setAttribute('disabled', '');
       } else {
         active = active.filter((i) => i !== index);
         target.setAttribute('inert', '');
         target.setAttribute('data-oversnap-item', '');
         pages[index]?.setAttribute('data-oversnap-page', '');
-        start && prev?.removeAttribute('disabled');
-        end && next?.removeAttribute('disabled');
       }
 
       active = [...new Set(active)].sort((a, b) => a - b);
+
       root.dispatchEvent(new CustomEvent('change', {
         detail: {
           items,
@@ -91,7 +176,7 @@ export default (root) => {
       }));
     });
   }, {
-    root: viewport ?? scroll,
+    root: viewport || scroll,
     rootMargin: '1px',
     threshold: 1,
   });
@@ -102,51 +187,26 @@ export default (root) => {
 
   if (prev) {
     prev.addEventListener('click', () => {
-      const alignment = align();
-
-      let index;
-
-      switch (alignment) {
-        case 'start':
-          index = active[0] - 1;
-          break;
-        case 'end':
-          index = active[active.length - 1] - 1;
-          break;
-        case 'center':
-          index = active[Math.floor(active.length / 2)] - 1;
-          break;
-      }
-
-      nav(index);
+      const index = getIndex() - 1;
+      setScroll(index);
     });
   }
 
   if (next) {
     next.addEventListener('click', () => {
-      const alignment = align();
-
-      let index;
-
-      switch (alignment) {
-        case 'start':
-          index = active[0] + 1;
-          break;
-        case 'end':
-          index = active[active.length - 1] + 1;
-          break;
-        case 'center':
-          index = active[Math.floor(active.length / 2)] + 1;
-          break;
-      }
-
-      nav(index);
+      const index = getIndex() + 1;
+      setScroll(index);
     });
+  }
+
+  if (prev || next) {
+    setDisabled();
+    scroll.addEventListener('scrollend', setDisabled);
   }
 
   pages.forEach((page, index) => {
      page.addEventListener('click', () => {
-       nav(index);
+       setScroll(index);
      });
   });
 };
